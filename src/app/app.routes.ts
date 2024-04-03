cf3f09e0d75eec43f5695d242077cb497fa6aff2
juliannaventer <juliannaventer@gmail.com>
2024-04-03 09:26:16 +0200

    feat: :sparkles: not-found and routing paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      // {
+      //   path: 'top',
+      //   component: TopMangaComponent,
+      // },
+      // {
+      //   path: 'manga/:slug',
+      //   component: SelectedMangaComponent,
+      // },
+      // {
+      //   path: 'manga/:slug/chapter/:hid',
+      //   component: SelectedMangaComponent,
+      // },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
